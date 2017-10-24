028dd9e88b69118d4f484014c70319981ee9af01
Brandon McGregor <iambrandonmcgregor@gmail.com>
2017-10-24 11:51:47 -0400

    Setup the products table.
    Added some mock data too.

--- src/app.js
+++ src/app.js
@@ -18,7 +18,63 @@ const app = Ractive({
     template,
     data: function () {
         return {
-            testWords: `Awesome! Really really awesome!`
+            testWords: `Awesome! Really really awesome!`,
+            products: [
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Maxi Dress - Floral',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'High Waist Jeans',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Grey Silk Blouse',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+                {
+                    name: 'Snapback Hat',
+                    type: 'Physical',
+                    price: 15.67,
+                    inventory: 200,
+                },
+            ],
         }
     },
 
