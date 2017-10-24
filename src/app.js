929159844f1739b7c656dce98c6f0cf422748767
Brandon McGregor <iambrandonmcgregor@gmail.com>
2017-10-24 11:01:15 -0400

    Setup basic Ractive utilization.
    Ractive will power the UI of our application.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
 
-window.console.info('Hello. Welcome to my website.');
+// Import external dependencies.
+import Ractive from 'ractive';
+import template from './app.html';
+import sass from './app.sass';
+
+// Caching references to global data.
+const users = window.users;
+const rawPosts = window.posts;
+const images = window.images;
+
+// Cache module-level variables.
+const maximumCharacters = 140;
+
+// Create our global Ractive application instance.
+const app = Ractive({
+    target: '#BrandonsFTProject',
+    template,
+    data: function () {
+        return {
+            testWords: `Awesome! Really really awesome!`
+        }
+    },
+});
+
+// Export the instance.
+export default app;
